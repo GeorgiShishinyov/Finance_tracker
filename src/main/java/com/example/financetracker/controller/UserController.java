b8a72df36787b7ed1f9f7cb64d0db2152654e990
camelliaiv <camelliaiv@gmail.com>
2023-04-14 16:25:42 +0300

    Implemented user registration functionality

--- src/main/java/com/example/financetracker/controller/UserController.java
+++ src/main/java/com/example/financetracker/controller/UserController.java
@@ -1,9 +1,23 @@
 package com.example.financetracker.controller;
 
+import com.example.financetracker.model.DTOs.RegisterDTO;
+import com.example.financetracker.model.DTOs.UserFullInfoDTO;
+import com.example.financetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController extends AbstractController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    public UserFullInfoDTO register(@RequestBody RegisterDTO dto){
+        return userService.register(dto);
+    }
 
 
 }
--- src/main/java/com/example/financetracker/model/entities/User.java
+++ src/main/java/com/example/financetracker/model/entities/User.java
@@ -1,13 +1,37 @@
 package com.example.financetracker.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private LocalDateTime dateOfBirth;
+
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
 }
