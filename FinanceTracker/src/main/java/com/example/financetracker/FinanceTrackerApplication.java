997904afd1e0c5679b6223160bd896238e404c8f
camelliaiv <camelliaiv@gmail.com>
2023-04-14 10:56:36 +0300

    Created project structure

---
+++ FinanceTracker/src/main/java/com/example/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,24 @@
+package com.example.financetracker;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
